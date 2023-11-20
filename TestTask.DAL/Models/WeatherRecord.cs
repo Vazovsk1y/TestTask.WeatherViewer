83c63adb9c04d59bc5316cd0d68c9dcba5d440c9
Vazovsk1y <anolrey.b@gmail.com>
2023-11-20 21:07:31 +0300

    -added database model.

---
+++ TestTask.DAL/Models/WeatherRecord.cs
@@ -0,0 +1,84 @@
+﻿namespace TestTask.DAL.Models;
+
+public class WeatherRecord
+{
+	public Guid Id { get; } 
+
+	public DateOnly MeasurementDate { get; set; }
+
+	public TimeOnly MeasurementTime {  set; get; }
+
+	/// <summary>
+	/// Температура воздуха, °C
+	/// </summary>
+	public double AirTemperature { get; set; } 
+
+	/// <summary>
+	/// Влажность воздуха, %
+	/// </summary>
+	public double AirHumidity { get; set; }
+
+	/// <summary>
+	/// Точка росы, °C
+	/// </summary>
+	public double DewPoint { get; set; }
+
+	/// <summary>
+	/// Атмосферное давление, мм рт. ст.
+	/// </summary>
+	public double AirPressure { get; set; }
+
+	/// <summary>
+	/// Скорость ветра, м/с
+	/// </summary>
+	public double? WindSpeed { get; set; }
+
+	/// <summary>
+	/// Облачность, %
+	/// </summary>
+	public double? Clouds { get; set; }
+
+	/// <summary>
+	/// Нижняя граница облачности, м
+	/// </summary>
+	public double LowCloudBoundary { get; set; }
+
+	/// <summary>
+	/// Горизонтальная видимость, км
+	/// </summary>
+	public double? HorizontalVisibility { get; set; }
+
+	/// <summary>
+	/// Природные явления
+	/// </summary>
+	public string? NaturalPhenomena { get; set; }
+
+	/// <summary>
+	/// Главное направление ветра Юг, Север, Восток, Запад
+	/// </summary>
+	public WindDirection MainWindDirection { get; set; }
+
+	/// <summary>
+	/// Дополнительное направление ветра Юго-восток, Северо-запад, Юго-запад, Северо-восток
+	/// </summary>
+	public WindDirection SecondaryWindDirection { get; set; }
+
+	public WeatherRecord() 
+	{
+		Id = Guid.NewGuid();
+	}
+}
+
+public enum WindDirection
+{
+	Undefined,
+	Calm,
+	North,
+	NorthEast,
+	East,
+	SouthEast,
+	South,
+	SouthWest,
+	West,
+	NorthWest
+}
