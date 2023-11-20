c8b4c207254e80f6f10750aeef6b6e7d5d41b813
Vazovsk1y <anolrey.b@gmail.com>
2023-11-20 22:41:32 +0300

    -added application dbContext implementation.

---
+++ TestTask.DAL/TestTaskSecondContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.DAL.Models;
+
+namespace TestTask.DAL;
+
+internal class TestTaskSecondContext : DbContext
+{
+	public DbSet<WeatherRecord> WeatherRecords { get; set; }
+	public TestTaskSecondContext(DbContextOptions options) : base(options)
+	{
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+
+		var weatherRecordBuilder = modelBuilder.Entity<WeatherRecord>();
+
+		weatherRecordBuilder.HasKey(x => x.Id);
+
+		weatherRecordBuilder.Property(e => e.MainWindDirection)
+			.IsRequired()
+			.HasConversion(e => e.ToString(), i => Enum.Parse<WindDirection>(i));
+
+		weatherRecordBuilder.Property(e => e.SecondaryWindDirection)
+			.IsRequired()
+			.HasConversion(e => e.ToString(), i => Enum.Parse<WindDirection>(i));
+	}
+}
