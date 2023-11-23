ffb1a9eff679ba55b9ce6ccf0c97858b1aad4f82
Vazovsk1y <anolrey.b@gmail.com>
2023-11-23 17:29:41 +0300

    -added base application view.

---
+++ TestTask.Web/Pages/WeatherUpload.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestTask.Web.Pages
+{
+    public class WeatherUploadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ TestTask.Web/Pages/WeatherView.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestTask.Web.Pages
+{
+	public class WeatherViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
